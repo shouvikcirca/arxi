0efb6827b33aa494eac1be4450ae6a7cb7cd3fe1
rhettscronfinkle <goldenfibo1123@gmail.com>
2020-06-29 21:06:51 +0530

    changed location of bashrc

--- appendtopath.py
+++ appendtopath.py
@@ -1,4 +1,5 @@
-f = open('/home/shouvik/.bashrc','a')
+import os
+f = open(os.path.join((os.path.expanduser('~'), '.bashrc'),'a')
 
 
 writeString = "\n\n\narxivdownload () {\n 	python3 /usr/local/bin/arxi.py $1\n}\nalias arxi='arxivdownload'\n"
