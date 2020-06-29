df37c8678db86d1d8183680db01f34b533e66d90
rhettscronfinkle <goldenfibo1123@gmail.com>
2020-06-29 21:22:17 +0530

    fixed syntax in appendtopath.py

--- appendtopath.py
+++ appendtopath.py
@@ -1,5 +1,5 @@
 import os
-f = open(os.path.join((os.path.expanduser('~'), '.bashrc'),'a')
+f = open(os.path.join((os.path.expanduser('~'), '.bashrc'),'a'))
 
 
 writeString = "\n\n\narxivdownload () {\n 	python3 /usr/local/bin/arxi.py $1\n}\nalias arxi='arxivdownload'\n"
